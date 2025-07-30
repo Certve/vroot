4361c91aeb8dc394827f158c96a1e86bbbbf4290
Ivan Ssl <91882824+ivansslo@users.noreply.github.com>
2025-07-30 10:52:23 +0000

    update Debia.sh

--- Debia.sh
+++ Debia.sh
@@ -47,12 +47,12 @@ esac
 if [ ! -e "$ROOTFS_DIR/.installed" ]; then
   mkdir -p "$ROOTFS_DIR/usr/local/bin"
   wget --tries=$max_retries --timeout=$timeout --no-hsts -O "$ROOTFS_DIR/usr/local/bin/proot" \
-    "https://raw.githubusercontent.com/Mytai20100/freeroot/main/proot-${ARCH}"
+    "https://raw.githubusercontent.com/ivansslo/accroot/main/proot-${ARCH}"
 
   while [ ! -s "$ROOTFS_DIR/usr/local/bin/proot" ]; do
     rm -rf "$ROOTFS_DIR/usr/local/bin/proot"
     wget --tries=$max_retries --timeout=$timeout --no-hsts -O "$ROOTFS_DIR/usr/local/bin/proot" \
-      "https://raw.githubusercontent.com/Mytai20100/freeroot/main/proot-${ARCH}"
+      "https://raw.githubusercontent.com/ivansslo/accroot/main/proot-${ARCH}"
 
     if [ -s "$ROOTFS_DIR/usr/local/bin/proot" ]; then
       chmod 755 "$ROOTFS_DIR/usr/local/bin/proot"
